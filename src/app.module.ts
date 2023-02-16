39ea0c70cc038b715de1401edcb1e5bc8cdd77a3
hiepph <bm.hcmc.hiep@modetour.com>
2023-02-16 21:29:30 +0700

    upload file

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './module/user/user.module';
 import { MovieModule } from './module/movie/movie.module';
 import { RoomModule } from './module/room/room.module';
+import { UploadFileModule } from './upload-file/upload-file.module';
 
 @Module({
-  imports: [UserModule, MovieModule, RoomModule],
+  imports: [UserModule, MovieModule, RoomModule, UploadFileModule],
 })
 export class AppModule {}
