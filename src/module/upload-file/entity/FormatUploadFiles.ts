6e198823cc3d7aff64ba4ca51d41d002b1782890
Hiepdz <hieule@Hieus-Mac-mini.local>
2023-03-25 12:21:51 +0700

    add upload files

---
+++ src/module/upload-file/entity/FormatUploadFiles.ts
@@ -0,0 +1,40 @@
+import { ApiBody } from '@nestjs/swagger';
+import { extname } from 'path';
+
+export const ApiMultiFile =
+  (fileName = 'file'): MethodDecorator =>
+  (target: any, propertyKey: string, descriptor: PropertyDescriptor) => {
+    ApiBody({
+      type: 'multipart/form-data',
+      required: true,
+      schema: {
+        type: 'object',
+        properties: {
+          [fileName]: {
+            type: 'array',
+            items: {
+              type: 'string',
+              format: 'binary',
+            },
+          },
+        },
+      },
+    })(target, propertyKey, descriptor);
+  };
+
+export const imageFileFilter = (req, file, callback) => {
+  if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
+    return callback(new Error('Only image files are allowed!'), false);
+  }
+  callback(null, true);
+};
+
+export const editFileName = (req, file, callback) => {
+  const name = file.originalname.split('.')[0];
+  const fileExtName = extname(file.originalname);
+  const randomName = Array(4)
+    .fill(null)
+    .map(() => Math.round(Math.random() * 16).toString(16))
+    .join('');
+  callback(null, `${name}-${randomName}${fileExtName}`);
+};
--- src/module/upload-file/upload-file.controller.ts
+++ src/module/upload-file/upload-file.controller.ts
@@ -4,17 +4,32 @@ import {
   UseInterceptors,
   UploadedFile,
   Body,
+  UploadedFiles,
+  Get,
+  Param,
+  Res,
 } from '@nestjs/common';
-import { FileInterceptor } from '@nestjs/platform-express';
+import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
 import { UploadFileService } from './upload-file.service';
-import { Express } from 'express';
+import { Express, Response } from 'express';
 import { UpLoadFileDto } from './dto/upload-dto';
 import { ApiConsumes, ApiTags } from '@nestjs/swagger';
+import {
+  ApiMultiFile,
+  editFileName,
+  imageFileFilter,
+} from './entity/FormatUploadFiles';
+import { diskStorage } from 'multer';
 
 @Controller('upload-file')
 @ApiTags('upload-file')
 export class UploadFileController {
   constructor(private uploadFileService: UploadFileService) {}
+
+  @Get('/get-file/:filename')
+  async getFile(@Param('filename') filename: any, @Res() res: Response) {
+    res.sendFile(filename, { root: './files' });
+  }
 
   @Post()
   @ApiConsumes('multipart/form-data')
@@ -27,5 +42,31 @@ export class UploadFileController {
       file.buffer,
       file.originalname,
     );
+  }
+  @Post('upload-multi')
+  @ApiConsumes('multipart/form-data')
+  @ApiMultiFile()
+  @UseInterceptors(
+    FilesInterceptor('file', 5, {
+      storage: diskStorage({
+        destination: './files',
+        filename: editFileName,
+      }),
+      fileFilter: imageFileFilter,
+    }),
+  )
+  uploadMultipleFiles(
+    @UploadedFiles()
+    files: any,
+  ) {
+    const response = [];
+    files.forEach((file) => {
+      const fileReponse = {
+        originalname: file.originalname,
+        filename: `http://localhost:8000/upload-file/get-file/${file.filename}`,
+      };
+      response.push(fileReponse);
+    });
+    return response;
   }
 }
